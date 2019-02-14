6b6765804b24c5c5e4a9b2014c2cc8c688263497
Niels Holt <niels@steenbuchholt.dk>
2019-02-14 08:36:28 +0100

    - New methods to get bsOptions from different settings to be edit in a bsForm - Update demo

--- README.md
+++ README.md
@@ -16,7 +16,9 @@ http://FCOO.github.io/fcoo-settings/demo/?settings={"test":66}  (see control pan
 ## Methods and Usage
 In namespace `window.fcoo` a object `settings` is created with the following methods
 
-### `fcoo.settings.add( options )`
+### Get and set settings
+
+#### `fcoo.settings.add( options )`
     options = {id, applyFunc, validator, defaultValue, onError )
 
 - `id [String]`: The id for the setting
@@ -27,22 +29,20 @@ In namespace `window.fcoo` a object `settings` is created with the following met
 - `globalEvents` (optional): Name(s) of events defined in [fcoo/fcoo-global-events](https://github.com/FCOO/fcoo-global-events) to be fired when the setting is changed.
 - `onError [Function( value, id )` (optional): Called if a new value is invalid according to validator
 
-
-
-### `fcoo.settings.set( id, value, reload )`
+#### `fcoo.settings.set( id, value, reload )`
 Sets the value for settings `id`
 If `value` is `undefined` => use the saved value
 The `applyFunc` is called with the new value
 If `reload==true` the pages is reloaded with the new settings
 
-### `fcoo.settings.get( id )`
+#### `fcoo.settings.get( id )`
 Return the current value (any type) of settings for `id`
 
-### `fcoo.settings.load()`
+#### `fcoo.settings.load()`
 Load the settings from the url `index.html?settings={"id":"value"}` or `localStorage`
 `load()` is called on loading the page, and normally it would not need to be called again
 
-### `fcoo.settings.save()`
+#### `fcoo.settings.save()`
 Save the settings to `localStorage`
 Is called on `set(...)`, and normally it would not need to be called individual
 
@@ -51,6 +51,18 @@ In some cases it is desirable to temporally overwrite the saved settings.
 This is done by calling the page with a parameter `?settings={..}&...` with a value = stringifyed JSON-object.
 Ex.: `index.html?settings={"test":99}`
 
+## Edit settings in a modal window
+To allow the user to edit the settings the following methods are defined to retrieve and display the modal.
+The modal contain an accordion with e card for each of the global events in [fcoo/fcoo-global-events](https://github.com/FCOO/fcoo-global-events)
+The different "Administrator" (see [FCOO/fcoo-web-application-settings](https://gitlab.com/FCOO/fcoo-web-application-settings)) will provide the content for each accordion card by calling `fcoo.settings.addModalContent(globalEventId, content)`
+The modal is displayed by calling `fcoo.settings.edit()`
+
+#### `fcoo.settings.addModalContent(globalEventId, content)`
+Add content to for accordion card with `id` and `content` as array of `bsOptions` (see [FCOO/jquery-bootstrap](https://github.com/FCOO/jquery-bootstrap))
+
+#### `fcoo.settings.edit([id])`
+Create and display the modal-window with edit-fields for the different groups given by the [global events](https://github.com/FCOO/fcoo-global-events)
+If `id` is given the card with the id is open by default
 
 ## Copyright and License
 This plugin is licensed under the [MIT license](https://github.com/FCOO/fcoo-settings/LICENSE).
--- dist/fcoo-settings.js
+++ dist/fcoo-settings.js
@@ -1,4 +1,110 @@
 /****************************************************************************
+    fcoo-settings-edit.js,
+
+    (c) 2016, FCOO
+
+    https://github.com/FCOO/fcoo-settings
+    https://github.com/FCOO
+
+    Methods for editing the settings
+
+    This packages create a modal window using jquery-bootstrap to edit
+    the settings.
+    The contwent of the modal window is devided into accordions - each for each of the
+    global events in fcoo-global-events
+
+    A method fcoo.settings.addModalContent(id, content) is created and is used by different
+    packages to add content to edit the settings
+
+    The id of the global-evnts are given as const:
+    fcoo.events.LANGUAGECHANGED
+    fcoo.events.DATETIMEFORMATCHANGED
+    fcoo.events.NUMBERFORMATCHANGED
+    fcoo.events.LATLNGFORMATCHANGED
+    fcoo.events.UNITCHANGED
+
+
+****************************************************************************/
+
+(function ($, window/*, document, undefined*/) {
+    "use strict";
+
+    //Create fcoo.settings-namespace
+    window.fcoo = window.fcoo || {};
+    var ns = window.fcoo.settings = window.fcoo.settings || {};
+
+    ns.modalContent = {};
+    ns.settingsId = [];
+
+    var originalData = {};
+
+    /*****************************************************
+    fcoo.settings.addModalContent(id, content)
+    *****************************************************/
+    ns.addModalContent = function(globalEventId, content){
+        ns.modalContent[globalEventId] = ns.modalContent[globalEventId] || [];
+
+        content = $.isArray(content) ? content : [content];
+        $.each( content, function( index, cont ){ ns.settingsId.push( cont.id ); });
+
+        ns.modalContent[globalEventId] = ns.modalContent[globalEventId].concat( content );
+    };
+
+
+    /*****************************************************
+    fcoo.settings.edit(id)
+    Create and display the modal window with setting
+    If id is given the corresponding accordion is open
+    *****************************************************/
+    ns.edit = function( id ){
+
+        //Create the modal
+        if (!ns.modalForm){
+            var list = [];
+            $.each(
+                [
+                    {id: window.fcoo.events.LANGUAGECHANGED,       header: {icon: 'fa-fw fa-comments',       text: {da: 'Sprog', en: 'Language'}} },
+                    {id: window.fcoo.events.DATETIMEFORMATCHANGED, header: {icon: 'fa-fw fa-calendar-alt',   text: {da: 'Tidszone, Dato og Tid', en: 'Time Zone, Date, and Time'}} },
+                    {id: window.fcoo.events.NUMBERFORMATCHANGED,   header: {                                 text: ['12',{da: 'Talformat', en: 'Number Format'}]} },
+                    {id: window.fcoo.events.LATLNGFORMATCHANGED,   header: {icon: 'fa-fw fa-map-marker-alt', text: {da: 'Positioner', en: 'Positions'}} },
+                    {id: window.fcoo.events.UNITCHANGED,           header: {icon: 'fa-fw fa-ruler',          text: {da: 'Enheder', en: 'Units'}} }
+                ],
+                function(index, accordInfo){
+                    if (ns.modalContent[accordInfo.id] && ns.modalContent[accordInfo.id].length)
+                        list.push({id: accordInfo.id, header: accordInfo.header, content: ns.modalContent[accordInfo.id]});
+                }
+            );
+
+            ns.modalForm = $.bsModalForm({
+                id     : 'fcoo-settings',
+                show   : false,
+                header : {icon: 'fa-cog', text: {da: 'Indstillinger', en:'Settings'}},
+                content: {type: 'accordion', list: list },
+                onSubmit: function( data ){
+                    $.each(data, function(id, value){
+                        if (value != originalData[id])
+                            ns.set(id, value);
+                    });
+                }
+            });
+        }
+
+        //Get data and save data
+        originalData = {};
+        $.each(ns.settingsId, function( index, id ){ originalData[id] = ns.get(id); });
+
+        //Open accordion with id
+        if (id)
+            ns.modalForm.$bsModal.find('form > .accordion').bsOpenCard(id);
+
+        ns.modalForm.edit(originalData);
+    };
+
+
+
+}(jQuery, this, document));
+;
+/****************************************************************************
     fcoo-settings.js, 
 
     (c) 2016, FCOO
